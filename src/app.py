4dd8f6ce949ed5abfa73bbaaf58472f57e2f4bde
Amogh Kini <amoghkini@gmail.com>
2023-07-08 03:27:33 +0530

    integration of web server

--- src/app.py
+++ src/app.py
@@ -1,23 +1,15 @@
-from pytube import YouTube
+from flask import Flask, render_template
 
-youtube_video_link = "https://www.youtube.com/watch?v=LXb3EKWsInQ"
+app = Flask(__name__)
 
-def on_download_process(stream, chunk, bytes_remaining):
-    bytes_downloaded = stream.filesize - bytes_remaining
-    percentage = bytes_downloaded * 100 / stream.filesize
-    
-    print(f"Downloading...{int(percentage)}")
-    
-youtube_video = YouTube(youtube_video_link)
+@app.route("/")
+def index():
+    return render_template("home.html")
 
-youtube_video.register_on_progress_callback(on_download_process)
-print("Title: ", youtube_video.title)
-print("No of views: ", youtube_video.views)
-print("Description: ", youtube_video.description)
 
-stream = youtube_video.streams.get_highest_resolution()
-print("Loading...")
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
-stream.download()
-print("Ok")
-
+if __name__ == "__main__":
+    app.run(debug=True)
